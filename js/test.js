ec32029813c50f25f4caa31e03d44a70d31c69be
mariozip <marius1950@gmail.com>
2025-08-08 20:18:13 +0000

    Modifica minima per testare l'allineamento del sito

--- js/test.js
+++ js/test.js
@@ -1,5 +1,4 @@
 function testFunction() {
   // Questa funzione mostra un semplice alert per confermare che è stata chiamata.
-  alert("La funzione di test è stata chiamata con successo!");
+  alert("La funzione di test è stata chiamata con successo! Funziona tutto!");
 }
-
